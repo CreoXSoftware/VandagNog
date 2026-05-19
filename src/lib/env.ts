d0d624f2d73315c5e3ee795483afdb384d8653e1
Nelis Brink <nelis@creox.co.za>
2026-05-19 12:50:51 +0200

    redirect fix

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -6,5 +6,7 @@ function required(key: string, val: string | undefined): string {
 export const env = {
   SUPABASE_URL: required('VITE_SUPABASE_URL', import.meta.env.VITE_SUPABASE_URL),
   SUPABASE_ANON_KEY: required('VITE_SUPABASE_ANON_KEY', import.meta.env.VITE_SUPABASE_ANON_KEY),
-  APP_URL: import.meta.env.VITE_APP_URL || window.location.origin,
+  APP_URL:
+    import.meta.env.VITE_APP_URL ||
+    window.location.origin + import.meta.env.BASE_URL.replace(/\/$/, ''),
 };
